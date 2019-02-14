c1f097fd05f79d28aec28f2a1eb276e81a2e83ad
jsy1218 <jsy1218@uw.edu>
2019-02-14 14:14:32 -0800

    fixed the bug with compress unordered chunks in multi threads

--- unordered_time_series_compressor.py
+++ unordered_time_series_compressor.py
@@ -1,18 +1,35 @@
+from enum import Enum
+import base_compression
+
+class Algorithm(Enum):
+    BoxCar = 1
+    SwingingDoor = 2
+    Hybrid = 3
+
 class UnorderedTimeSeriesCompressor:
-    def __init__(self, data_size, base_compressor):
+    def __init__(self, data_size, algorithm):
         # This is prototype code and doesn't validate arguments
-        self.data_size = data_size * 4
-        self.base_compressor = base_compressor
+        
+        if algorithm == Algorithm.BoxCar:
+            self.base_compressor = BoxCarCompression(data_size / 2)
+            self.final_compressor = BoxCarCompression(data_size)
+        elif algorithm == Algorithm.SwingingDoor:
+            self.base_compressor = SwingingDoorCompression(data_size / 2)
+            self.final_compressor = SwingingDoorCompression(data_size)
+        else:
+            self.base_compressor = HybridCompression(data_size / 2)
+            self.final_compressor = HybridCompression(data_size)
+            
         self._multithreading_threshold = 10000
         self._chunks = {}
         
-    def _compress_chunk(self, time_series_chunk):                
+    def _compress_chunk(self, time_series_chunk, depth = 0):               
         if len(time_series_chunk) <= self._multithreading_threshold:
             ordered_time_series_chunk = OrderedDict(sorted(time_series_chunk.items(), key=lambda t: t[0]))
             size = len(self._chunks)
             self._chunks[size] = self.base_compressor._run(ordered_time_series_chunk)
         else:
-            thread_size = (len(time_series) + self._multithreading_threshold - 1) // self._multithreading_threshold
+            thread_size = (len(time_series_chunk) + self._multithreading_threshold - 1) // self._multithreading_threshold
             threads = [None] * thread_size
             
             single_chunk = {}
@@ -28,7 +45,8 @@ class UnorderedTimeSeriesCompressor:
                 count += 1
             
             for i in range(thread_size):
-                threads[i] = threading.Thread(target=_compress_chunk, args=(results, chunk_collections[i]))
+                
+                threads[i] = threading.Thread(target=self._compress_chunk, args=(chunk_collections[i], depth + 1))
                 threads[i].start()
                 threads[i].join()
                 
@@ -37,39 +55,21 @@ class UnorderedTimeSeriesCompressor:
             
         if len(self._chunks) == 0:
             return merged_chunks
-        
-        merged_chunks.update(self._chunks)
-                
-        iteration = 1
-        
-        while len(merged_chunks) > 1:
-            intermediate_chunks = {}
-            intermediate_index = 0
-            
-            merged_chunks_times = iter(merged_chunks.keys())
-
-            next_merged_chunk_time = next(merged_chunks_times, None)
-            next_next_merged_chunk_time = next(merged_chunks_times, None)
-            
-            is_even = False
-            
-            while next_merged_chunk_time is not None and next_next_merged_chunk_time is not None:
-                merged_chunk = self.__merge_sorted(merged_chunks[next_merged_chunk_time], merged_chunks[next_next_merged_chunk_time])
-                intermediate_chunks[intermediate_index] = (merged_chunk)
-                intermediate_index += 1
-                
-                next_merged_chunk_time = next(merged_chunks_times, None)
-                next_next_merged_chunk_time = next(merged_chunks_times, None)
-            
-            if next_merged_chunk_time is not None:        
-                intermediate_chunks[intermediate_index] = merged_chunks[next_merged_chunk_time]
-                
-            merged_chunks.clear()
-            merged_chunks.update(intermediate_chunks)
                         
-            iteration += 1
+        index = 0
+                
+        while index in self._chunks and index + 1 in self._chunks:
+            merged_chunk = self.__merge_sorted(self._chunks[index], self._chunks[index + 1])
+            merged_chunks.update(merged_chunk)
+
+            index += 2
+
+        if index in self._chunks:        
+            merged_chunks.update(self._chunks[index])
         
-        compressed_data = self.base_compressor._run(merged_chunks[0])
+        self._chunks.clear()
+                                        
+        compressed_data = self.final_compressor._run(merged_chunks)
         
         return compressed_data
     
@@ -78,7 +78,7 @@ class UnorderedTimeSeriesCompressor:
         
         chunk_1_times = iter(chunk_1.keys())
         chunk_2_times = iter(chunk_2.keys())
-        
+                
         next_chunk_1 = next(chunk_1_times, None)
         next_chunk_2 = next(chunk_2_times, None)
         
